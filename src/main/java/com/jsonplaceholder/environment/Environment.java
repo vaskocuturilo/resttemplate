30cee18a61b43a54186820cb8ee258aa4fc9312e
Vasko <vaskocuturilo@rambler.ru`>
2019-12-04 13:53:25 +0300

    Feat : Environment : Added method for check operation system.
    Commit at 04/12/2019

---
+++ src/main/java/com/jsonplaceholder/environment/Environment.java
@@ -0,0 +1,20 @@
+package com.jsonplaceholder.environment;
+
+/**
+ * The class Environment.
+ */
+public class Environment {
+
+    /**
+     * Method isCheckOperatingSystem.
+     * If method return true, that is automation scripts to run on OS Unix or Linux.
+     * If method return false, that is automation scripts to run on OS Windows or Mac.
+     *
+     * @return the boolean.
+     */
+    public boolean isCheckOperationSystem() {
+        String typeOfOperationSystem = System.getProperty("os.name");
+
+        return (typeOfOperationSystem.contains("nix") || typeOfOperationSystem.contains("nux"));
+    }
+}
